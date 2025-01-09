b03a5412724474c8c2c2ca00b6cd280c6e2b46af
Leonardo V Pereira <leonardo.vpereira@al.infnet.edu.br>
2025-01-09 19:56:30 -0300

    conceito básicos de tipos e variaveis

--- sintaxe-java/src/tipos-variaveis/aula1/App.java
+++ sintaxe-java/src/tipos-variaveis/aula1/App.java
@@ -1,5 +1,18 @@
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws Exception {       
+        // Abaixo um ex de uma conversão impossível de dado,
+        // pois o tipo int exige mais meméria do que o tipo short 
+        
+        //short numeroCurto = 1;
+        //int numeroNormal = numeroCurto;
+        //short numeroCurto2 = numeroNormal;
+
+        //Solução pare esse problema:
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.print(numeroCurto2);
     }
 }
